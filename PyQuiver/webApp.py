cd63f4921557a2a659a4c33946ea8fda4222395f
Andrew Ly <lydinhandrew@gmail.com>
2025-03-07 13:06:38 -0800

    Removed validate_data, started adding to calculate_kie

--- PyQuiver/webApp.py
+++ PyQuiver/webApp.py
@@ -21,35 +21,37 @@ def kie():
 
 @app.route('/kie_calculate', methods=['POST'])
 def calculate_kie():
-    
+    temperature = request.form.get('temperature')
+
+    return f"KIE Calculation completed at {temperature}"
 
 # Load the EIE page
 @app.route('/eie')
 def eie():
     return render_template('eie.html')
 
-@app.route('/validate_data', methods=['POST'])
-def validate_data():
-    # case 1: any one of the files is not selected and uploaded
-    if 'config_file' not in request.files or 'file1' not in request.files or 'file2' not in request.files:
-        return "Missing file", 400
+# @app.route('/validate_data', methods=['POST'])
+# def validate_data():
+#     # case 1: any one of the files is not selected and uploaded
+#     if 'config_file' not in request.files or 'file1' not in request.files or 'file2' not in request.files:
+#         return "Missing file", 400
     
-    files = {
-        'config_file': request.files['config_file'],
-        'file1': request.files['file1'], 
-        'file2': request.files['file2']
-    }
+#     files = {
+#         'config_file': request.files['config_file'],
+#         'file1': request.files['file1'], 
+#         'file2': request.files['file2']
+#     }
 
-    for key, file in files.items():
-        # case 2: dialog to select file comes up, but nothing is uploaded
-        if file.filename == '':
-            return f"No selected file for {key}", 400
+#     for key, file in files.items():
+#         # case 2: dialog to select file comes up, but nothing is uploaded
+#         if file.filename == '':
+#             return f"No selected file for {key}", 400
         
-        # case 3: invalid file format uploaded
-        if not any(file.filename.endswith(ext) for ext in VALID_EXTENSIONS[key]):
-            return f"Invalid file format for {key}. Expected one of {VALID_EXTENSIONS[key]}"
+#         # case 3: invalid file format uploaded
+#         if not any(file.filename.endswith(ext) for ext in VALID_EXTENSIONS[key]):
+#             return f"Invalid file format for {key}. Expected one of {VALID_EXTENSIONS[key]}"
 
-    return "Files uploaded successfully!", 200
+#     return "Files uploaded successfully!", 200
 
 
 
