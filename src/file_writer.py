ac885b7e0370321fa8396ebad7b1d16b330d3907
dsabet <dianvash82@gmail.com>
2025-06-04 19:10:01 -0700

    Removed unused files: autoquiver.py, file_writer.py, utility.py, orca.py

--- src/file_writer.py
+++
@@ -1,7 +0,0 @@
-class FileWriter:
-    def __init__(self, filepath):
-        self.filepath = filepath
-
-    def write_script(self, script_content):
-        with open(self.filepath, 'a') as file:
-            file.write(script_content + '\n')
