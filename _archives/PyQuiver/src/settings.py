b5528466ae4b9d9aca00a710458bff1cf8af3d8f
ggrazioli <18152639+ggrazioli@users.noreply.github.com>
2026-06-10 20:07:18 -0700

    Remove archived code directory

--- _archives/PyQuiver/src/settings.py
+++
@@ -1,6 +0,0 @@
-# DEBUG global variable
-#  3 = dump files into cwd
-#  2 = extra verbose - corresponds to -v flag on quiver or -vv flag on auto
-#  1 = reasonable level of printing
-#  0 = minimal printing
-DEBUG = 0
